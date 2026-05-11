6a121348faf01c7eb04c854ed167caf057da6d74
CentrovisonThailand <CentrovisonThailand@gmail.com>
2026-05-11 11:46:57 +0700

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import FruitNinjaGame from '@/components/FruitNinja';
+
+export default function Home() {
+  return (
+    <main className="bg-black min-h-screen">
+      <FruitNinjaGame />
+    </main>
+  );
+}
